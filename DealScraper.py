bbe201502886e935693f8264d474942b445d278d
elliot-tio <etjinzo@gmail.com>
2019-10-10 23:59:06 -0700

    initial scraping and emailing done

---
+++ DealScraper.py
@@ -0,0 +1,83 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+from email.mime.text import MIMEText
+from vars import *
+
+page = 1
+base = 'https://forums.redflagdeals.com'
+url = 'https://forums.redflagdeals.com/hot-deals-f9/' + str(page)
+terms = ['144p', '2560', '1440', '27"' , 'Dell', 'U27']
+matches = []
+pages = []
+
+maxPagesRequest = requests.get(url, headers={"User-Agent": "Mozilla Firefox"})
+maxPagesSoup = BeautifulSoup(maxPagesRequest.text, 'html5lib')
+maxPages = int(maxPagesSoup.find(class_='pagination_menu_trigger').text.split()[-1])
+
+while page <= maxPages:
+    print('Parsing page ' + str(page) + '...')
+    response = requests.get(url, headers={"User-Agent": "Mozilla Firefox"})
+    if response.status_code != 200:
+        print('Error! Response status ' + str(response.status_code))
+        break
+
+    soup = BeautifulSoup(response.text, 'html5lib')
+    links = soup.findAll(class_='topic_title_link')
+
+    for term in terms:
+        for link in links:
+            if term in link.text.strip():
+                if link.text.strip() not in matches:
+                    print('Match!')
+                    matches.append(link.text.strip())
+                    pages.append({"text": link.text.strip(), "url": base + link['href']})
+    
+    page += 1
+    url = 'https://forums.redflagdeals.com/hot-deals-f9/' + str(page)
+    print('Moving to page ' + str(page))
+    print(str(len(matches)) + ' found so far...\n')
+
+print('Creating Server')
+server = smtplib.SMTP_SSL('smtp.gmail.com')
+print('Logging in...')
+server.login(sender, password)
+print('Constructing Message...')
+
+htmlList = """\
+<ul>
+"""
+for page in pages:
+    htmlList += """\
+        <li>
+            <a href="$(url)">$(text)</a>
+        </li>
+    """
+    htmlList = htmlList.replace("$(url)", page['url'])
+    htmlList = htmlList.replace("$(text)", page['text'])
+
+htmlList += """\
+</ul>
+"""
+
+body = """\
+<html>
+    <body>
+        <p>Here's a list of interesting deals</p>
+        $(htmlList)
+    </body>
+</html>
+"""
+
+body = body.replace("$(htmlList)", htmlList)
+msg = MIMEText(body, 'HTML')
+
+msg['Subject'] = "Found some Deals!"
+msg['From'] = sender
+msg['To'] = receiver
+
+print('Sending Message...')
+server.sendmail(sender, [receiver], msg.as_string())
+print('Message Sent!')
+server.quit()
+print('Program terminated.')
